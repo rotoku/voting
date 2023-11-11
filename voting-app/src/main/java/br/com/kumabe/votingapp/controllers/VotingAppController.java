a40c13c2eae875ebda2efe912d5a8d75df3115f0
Rodrigo Kumabe <rodrigo.kumabe@gmail.com>
2023-11-11 08:01:01 -0300

    feat: voting-app

--- voting-app/src/main/java/br/com/kumabe/votingapp/controllers/VotingAppController.java
+++ voting-app/src/main/java/br/com/kumabe/votingapp/controllers/VotingAppController.java
@@ -1,14 +1,11 @@
 package br.com.kumabe.votingapp.controllers;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
