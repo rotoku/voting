23d723dbe2ca6ae7cef42290ea7dbf631951a06e
Rodrigo Kumabe <rodrigo.kumabe@gmail.com>
2023-11-12 11:13:24 -0300

    feat: voting-app

--- voting-worker/src/main/java/br/com/kumabe/votingworker/SpringApp.java
+++ voting-worker/src/main/java/br/com/kumabe/votingworker/SpringApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VotingWorkerApplication {
+public class SpringApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VotingWorkerApplication.class, args);
+		SpringApplication.run(SpringApp.class, args);
 	}
 
 }
